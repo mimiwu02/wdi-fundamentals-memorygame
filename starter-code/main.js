520b3187db025af6e129319746a508b29e917a92
Mimi Wu <MimiWu@Mimis-MacBook-Pro.local>
2016-06-11 18:23:31 -0400

    creating game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,28 @@
-console.log("JS file is connected to HTML! Woo!")
+//console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+//var cardOne = "queen"
+//var cardTwo = "queen"
+//var cardThree = "king"
+//var cardFour = "king"
 
-if (cardTwo===cardFour) 
-	{alert("You found a match!")}
-else {alert("Sorry, try again.")}; 
+//if (cardTwo===cardFour) 
+	//{alert("You found a match!")}
+//else {alert("Sorry, try again.")}; 
+
+//Get an element with the id of game-board and set it to a variable.
+var board= document.getElementById('game-board');
+
+//Create a function called createBoard in your Javascript file. 
+function createBoard() {
+// create a for loop that makes an HTML element for each card. 
+
+ for (var i=0; i<cards.length; i++) {
+ 	var cardElement = document.createElement('div'); 
+
+// add a class
+cardElement.className = 'card';
+
+//Append each newly created card to the div that has the class board
+board.appendChild(cardElement);
+ 	}
+}
